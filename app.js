01944dd5eaff54ce868bcb3b1c53cb67ce9108d6
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-08-02 16:24:09 +0530

    Added check for existing user

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const routes = require('./server/router/routes');
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const port = 3000;
 
 const app = express();
+
+app.use(express.json());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -14,17 +14,41 @@ exports.viewSignUp = (req,res)=>{
     res.sendFile(path.join(__dirname,'../..','views','signup.html'));
 }
 
+exports.checkUser = (req,res) => {
+    
+};
+
 exports.doSignUp = (req,res)=>{
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    const query = 'INSERT INTO User (Name, Email, Password) VALUES (?,?,?)'
-    pool.execute(query, [name,email,password],(err,result)=>{
+
+    pool.execute('SELECT COUNT(*) AS count FROM User WHERE Email = ?',[email],(err,results) =>{
         if(err){
-            console.log(err);
-            return
+            console.log('Error querying database',err);
         }
-        console.log('data inserted successfully');
-        res.sendFile(path.join(__dirname,'../..','views','home.html'));
+
+        const userExists = results[0].count>0;
+        if(userExists){
+            return res.send(`
+            <script>
+                alert('User already exists');
+                window.history.back();
+            </script>
+        `);
+
+        }else{
+
+        const query = 'INSERT INTO User (Name, Email, Password) VALUES (?,?,?)'
+        pool.execute(query, [name,email,password],(err,result)=>{
+            if(err){
+                console.log(err);
+                return
+            }
+            console.log('data inserted successfully');
+            res.sendFile(path.join(__dirname,'../..','views','home.html'));
+        });
+    };
     });
-};
+}
+
